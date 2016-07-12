4b1068e756146f6a5dad0ff419a995f8b649573a
minabe <minabe@bringup.jp>
2016-07-12 15:01:34 +0900

    fix index

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Circle Ci Deploy</title>
+    <title>Circle Ci Deploy T</title>
 </head>
 <body>
-    <h1>Circle Ci Deploy</h1>
+    <h1>Circle Ci Deploy T</h1>
 </body>
 </html>
