c7957e27340dd0fceb042e264eab0ff9fcd6172e
minabe <minabe@bringup.jp>
2016-07-14 13:16:20 +0900

    fix index deploy test

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Circle Ci Test</title>
+    <title>Circle Ci Deploy Test</title>
 </head>
 <body>
-    <h1>Circle Ci Test</h1>
+    <h1>Circle Ci Deploy Test</h1>
 </body>
 </html>
