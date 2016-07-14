a6042709c9d7a9a2bfcdb692379b5d4154b5ea0e
minabe <minabe@bringup.jp>
2016-07-14 09:58:18 +0900

    add logs .htaccess

--- hook.php
+++ hook.php
@@ -94,6 +94,5 @@ switch($mode){
     checkSecret($hookSecret);
     checkContentType();
     $payload = json_decode(getJson(), true);
-    file_put_contents(LOG_PATH.'hook.log', getJson()."\n", FILE_APPEND|LOCK_EX);
     triggerEvent($payload);
 }
